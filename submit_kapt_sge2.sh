81c7b7643e3e90999b1ae140d40c60148c2e8347
felipevzps <felipe.vzps@gmail.com>
2025-06-26 15:08:28 -0300

    bash script to submit nextflow with configuration 2 on sge

---
+++ submit_kapt_sge2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -pe smp 1
+#$ -l h=neotera
+#$ -t 1
+
+samples=`ls -1 samples_test/*.csv | head -n $SGE_TASK_ID | tail -n1`
+bioproject=`basename ${samples/.csv}`
+
+module load nextflow
+
+/usr/bin/time -v nextflow run workflows/KAPT.nf -c config/nextflow_sge2.config -resume -profile sge --samples_csv $samples --output_dir "../results/${bioproject}" --report_dir "report/${bioproject}"
