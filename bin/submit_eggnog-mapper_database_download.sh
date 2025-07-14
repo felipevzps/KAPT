792c0b4bdbedb69f51eb06a9a22b5bcebbc0c95d
felipevzps <felipe.vzps@gmail.com>
2025-07-14 16:50:27 -0300

    move script to download eggNOG-mapper database

---
+++ bin/submit_eggnog-mapper_database_download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -pe smp 1
+#$ -t 1
+
+module load eggnog-mapper/2.1.9
+
+database_path="/Storage/data2/felipe.peres/KAPT/eggnog_database/"
+
+# tutorial: https://github.com/eggnogdb/eggnog-mapper/wiki/eggNOG-mapper-v2.1.5-to-v2.1.12#user-content-v219
+
+# downloading eggNOG-mapper databases
+/usr/bin/time -v download_eggnog_data.py --data_dir $database_path -y
