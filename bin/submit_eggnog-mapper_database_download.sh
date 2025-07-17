4241ec17b72a92304fe58ac46ddc80ad1cf0be62
felipevzps <felipe.vzps@gmail.com>
2025-07-17 00:15:25 -0300

    rename database path for better readability

--- bin/submit_eggnog-mapper_database_download.sh
+++ bin/submit_eggnog-mapper_database_download.sh
@@ -8,9 +8,9 @@
 
 module load eggnog-mapper/2.1.9
 
-database_path="/Storage/data2/felipe.peres/KAPT/eggnog_database/"
+eggnog_db_path="/Storage/data2/felipe.peres/KAPT/eggnog_database/"
 
 # tutorial: https://github.com/eggnogdb/eggnog-mapper/wiki/eggNOG-mapper-v2.1.5-to-v2.1.12#user-content-v219
 
 # downloading eggNOG-mapper databases
-/usr/bin/time -v download_eggnog_data.py --data_dir $database_path -y
+/usr/bin/time -v download_eggnog_data.py --data_dir $eggnog_db_path -y
