099d1165fa93885ae619c957df2cc55ad8000189
felipevzps <felipe.vzps@gmail.com>
2025-07-20 01:59:28 -0300

    update submission script with correct variables

--- bin/submit_kapt_sge.sh
+++ bin/submit_kapt_sge.sh
@@ -6,9 +6,9 @@
 #$ -pe smp 1
 #$ -t 1
 
-samples=`ls -1 samples_test/*.csv | head -n $SGE_TASK_ID | tail -n1`
+samples=`ls -1 samples/*.csv | head -n $SGE_TASK_ID | tail -n1`
 bioproject=`basename ${samples/.csv}`
 
 module load nextflow
 
-/usr/bin/time -v nextflow run workflows/KAPT.nf -c config/nextflow_sge2.config -resume -profile sge --samples_csv $samples --output_dir "../results/${bioproject}" --report_dir "report/${bioproject}"
+/usr/bin/time -v nextflow run workflows/KAPT.nf -c config/nextflow.config -resume -profile sge --samples_csv $samples --output_dir "../results/${bioproject}" --report_dir "report/${bioproject}"
